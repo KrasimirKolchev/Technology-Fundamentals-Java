d38bc11bccfe082c86fba47aaab3cbe031de7c28
KrasimirKolchev <49152390+KrasimirKolchev@users.noreply.github.com>
2019-05-07 13:11:11 +0300

    Update problem6.java

--- FinalExam/problem6.java
+++ FinalExam/problem6.java
@@ -19,17 +19,40 @@ public class problem6 {
                 unfedAnimals.get(info[3]).put(info[1], unfedAnimals.get(info[3]).get(info[1]) + Integer.parseInt(info[2]));
 
             } else if (info[0].equals("Feed")) {
-                unfedAnimals.get(info[3]).put(info[1], unfedAnimals.get(info[3]).get(info[1]) - Integer.parseInt(info[2]));
 
-                if (unfedAnimals.get(info[3]).get(info[1]) <= 0) {
-                    unfedAnimals.get(info[3]).remove(info[1]);
-                    if (unfedAnimals.get(info[3]).size() == 0) {
-                        unfedAnimals.remove(info[3]);
+
+                if (unfedAnimals.containsKey(info[3])) {
+                    if (unfedAnimals.get(info[3]).containsKey(info[1])) {
+                        unfedAnimals.get(info[3]).put(info[1], unfedAnimals.get(info[3]).get(info[1]) - Integer.parseInt(info[2]));
+
+                        if (unfedAnimals.get(info[3]).get(info[1]) <= 0) {
+                            unfedAnimals.get(info[3]).remove(info[1]);
+                            if (unfedAnimals.get(info[3]).size() == 0) {
+                                unfedAnimals.remove(info[3]);
+                            }
+                            System.out.println(String.format("%s was successfully fed", info[1]));
+                        }
                     }
-                    System.out.println(String.format("%s was successfully fed", info[1]));
                 }
             }
         }
+        Map<String, Integer> animals = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Map<String, Integer>> entry : unfedAnimals.entrySet()) {
+            for (Map.Entry<String, Integer> item : entry.getValue().entrySet()) {
+                animals.put(item.getKey(), item.getValue());
+            }
+        }
+
+        System.out.println("Animals:");
+        animals.entrySet().stream().sorted((a, b) -> {
+            int sort = Integer.compare(b.getValue(), a.getValue());
+            if (sort == 0) {
+                sort = a.getKey().compareTo(b.getKey());
+            }
+            return sort;
+        } )
+                .forEach(e -> System.out.println(String.format("%s -> %dg", e.getKey(), e.getValue())));
 
         System.out.println("Areas with hungry animals:");
         unfedAnimals.entrySet().stream().sorted((e1, e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()))
